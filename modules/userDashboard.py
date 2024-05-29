64733d97a5f7562b16d21af14eac0f2e7a67a36e
Amin Gharibi <gharibi8364@gmail.com>
2024-05-30 01:49:39 +0330

    create favorite movies tab inital design template in user dashboard

--- modules/userDashboard.py
+++ modules/userDashboard.py
@@ -19,13 +19,13 @@ class UserDashboard(ctk.CTkFrame):
         navbar_frame = ctk.CTkFrame(self)
         navbar_frame.grid(row=1, column=0, sticky='n', pady=5)
 
-        self.my_profile_button = ctk.CTkButton(navbar_frame, text="My Profile", command=lambda: self.load_my_profile_section(dynamic_content_frame))
+        self.my_profile_button = ctk.CTkButton(navbar_frame, text="My Profile", command=lambda: self.load_my_profile_tab(dynamic_content_frame))
         self.my_profile_button.grid(row=0, column=0, padx=20, pady=20)
 
         self.my_comments_button = ctk.CTkButton(navbar_frame, text="My Comments", command=lambda: self.load_my_comments_tab(dynamic_content_frame))
         self.my_comments_button.grid(row=0, column=1, padx=20, pady=20)
 
-        self.my_favorite_movies_button = ctk.CTkButton(navbar_frame, text="My Favorite Movies")
+        self.my_favorite_movies_button = ctk.CTkButton(navbar_frame, text="My Favorite Movies", command=lambda: self.load_my_favorite_movies_tab(dynamic_content_frame))
         self.my_favorite_movies_button.grid(row=0, column=2, padx=20, pady=20)
 
         self.my_favorite_articles_button = ctk.CTkButton(navbar_frame, text="My Favorite Articles")
@@ -44,7 +44,7 @@ class UserDashboard(ctk.CTkFrame):
         print(file_name)
         return file_name
 
-    def load_my_profile_section(self, parent):
+    def load_my_profile_tab(self, parent):
         # disable target tab button and enable other tabs button
         self.my_profile_button.configure(state='disabled')
         self.my_comments_button.configure(state='normal')
@@ -140,3 +140,123 @@ class UserDashboard(ctk.CTkFrame):
         # create each comments template from the backend
         for index, comment in enumerate(comments):
             Comment(parent, comment, fg_color='gray23').grid(row=index, column=0, sticky='ew', padx=20, pady=20)
+
+    def load_my_favorite_movies_tab(self, parent):
+        from modules.itemBox import ItemBox
+        from modules.moviePage import MoviePage
+        from math import floor
+
+        # disable target tab button and enable other tabs button
+        self.my_favorite_movies_button.configure(state='disabled')
+        self.my_profile_button.configure(state='normal')
+        self.my_comments_button.configure(state='normal')
+        self.my_favorite_articles_button.configure(state='normal')
+
+        # empty widgets in the parent
+        for widget in parent.winfo_children():
+            widget.destroy()
+
+        movies_details = [
+            {
+                "id": 0,
+                "title": "After Life",
+                "description": "This movie is so amazing and i would definitely suggest you to watch this super amazing movie. seriously i mean it hatman nagash konid",
+                "cover": "images/imdb_logo.png",
+                "genre": "romance, comedy",
+                "releaseDate": "23/05/2024",
+                "countries": "United States Of America, United Arab Emirates",
+                "languages": "English(US)",
+                "budget": 100_000_000,
+                "rate": 5
+            },
+            {
+                "id": 1,
+                "title": "After Zendegi",
+                "description": "This movie is so amazing and i would definitely suggest you to watch this super amazing movie.",
+                "cover": "images/imdb_logo.png",
+                "genre": "romance, comedy",
+                "releaseDate": "23/05/2024",
+                "countries": "United States Of America, United Arab Emirates",
+                "languages": "English(US)",
+                "budget": 100_000_000,
+                "rate": 4.5
+            },
+            {
+                "id": 2,
+                "title": "After Jendegi",
+                "description": "This movie is so amazing.",
+                "cover": "images/imdb_logo.png",
+                "genre": "romance, comedy",
+                "releaseDate": "23/05/2024",
+                "countries": "United States Of America, United Arab Emirates",
+                "languages": "English(US)",
+                "budget": 100_000_000,
+                "rate": 1.2
+            },
+            {
+                "id": 3,
+                "title": "After Life",
+                "description": "This movie is so amazing and i would definitely suggest you to watch this super amazing movie. seriously i mean it hatman nagash konid",
+                "cover": "images/imdb_logo.png",
+                "genre": "romance, comedy",
+                "releaseDate": "23/05/2024",
+                "countries": "United States Of America, United Arab Emirates",
+                "languages": "English(US)",
+                "budget": 100_000_000,
+                "rate": 5
+            },
+            {
+                "id": 4,
+                "title": "After Life",
+                "description": "This movie is so amazing and i would definitely suggest you to watch this super amazing movie. seriously i mean it hatman nagash konid",
+                "cover": "images/imdb_logo.png",
+                "genre": "romance, comedy",
+                "releaseDate": "23/05/2024",
+                "countries": "United States Of America, United Arab Emirates",
+                "languages": "English(US)",
+                "budget": 100_000_000,
+                "rate": 5
+            },
+            {
+                "id": 5,
+                "title": "After Life",
+                "description": "This movie is so amazing and i would definitely suggest you to watch this super amazing movie. seriously i mean it hatman nagash konid",
+                "cover": "images/imdb_logo.png",
+                "genre": "romance, comedy",
+                "releaseDate": "23/05/2024",
+                "countries": "United States Of America, United Arab Emirates",
+                "languages": "English(US)",
+                "budget": 100_000_000,
+                "rate": 5
+            },
+            {
+                "id": 6,
+                "title": "After Life",
+                "description": "This movie is so amazing and i would definitely suggest you to watch this super amazing movie. seriously i mean it hatman nagash konid",
+                "cover": "images/imdb_logo.png",
+                "genre": "romance, comedy",
+                "releaseDate": "23/05/2024",
+                "countries": "United States Of America, United Arab Emirates",
+                "languages": "English(US)",
+                "budget": 100_000_000,
+                "rate": 5
+            },
+            {
+                "id": 7,
+                "title": "After Life",
+                "description": "This movie is so amazing and i would definitely suggest you to watch this super amazing movie. seriously i mean it hatman nagash konid",
+                "cover": "images/imdb_logo.png",
+                "genre": "romance, comedy",
+                "releaseDate": "23/05/2024",
+                "countries": "United States Of America, United Arab Emirates",
+                "languages": "English(US)",
+                "budget": 100_000_000,
+                "rate": 5
+            }
+        ]
+
+        holder_frame = ctk.CTkFrame(parent)
+        holder_frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+
+        for index, movie in enumerate(movies_details):
+            ItemBox(holder_frame, target_fg_color=['gray86', 'gray17'], details_page=MoviePage, item=movie).grid(row=floor(index / 4) + 1, column=(index % 4))
