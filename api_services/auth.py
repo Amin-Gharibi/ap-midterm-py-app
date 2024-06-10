92f3fa8721cf1670f8787e148cf169a52fcb3cb4
Amin Gharibi <gharibi8364@gmail.com>
2024-06-10 23:28:35 +0330

    modify auth apis to handle otp verification

--- api_services/auth.py
+++ api_services/auth.py
@@ -12,9 +12,21 @@ def register(email: str,
 
     res = req.post(f'{getenv('BASE_URL')}/auth/register', json=sending_data)
 
+    return {"message": res.json()['message'] or 'Something Went Wrong!'}
+
+
+def validate_register_otp(email: str,
+                          username: str,
+                          password: str,
+                          role: str,
+                          code: str):
+
+    sending_data = {key: value for key, value in locals().items()}
+
+    res = req.post(f'{getenv('BASE_URL')}/auth/register/otp', json=sending_data)
+
     if res.status_code == 201:
         save_access_token(res.json()['accessToken'])
-        return {"message": "Registered Successfully!"}
 
     return {"message": res.json()['message'] or 'Something Went Wrong!'}
 
@@ -24,9 +36,16 @@ def login(identifier: str, password: str):
 
     res = req.post(f'{getenv('BASE_URL')}/auth/login', json=sending_data)
 
+    return {"message": res.json()['message'] or 'Something Went Wrong!'}
+
+
+def validate_login_otp(identifier: str, password: str, code: str):
+    sending_data = {key: value for key, value in locals().items()}
+
+    res = req.post(f'{getenv('BASE_URL')}/auth/login/otp', json=sending_data)
+
     if res.status_code == 200:
         save_access_token(res.json()['accessToken'])
-        return {"message": "Login Successfully!"}
 
     return {"message": res.json()['message'] or 'Something Went Wrong!'}
 
